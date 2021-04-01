4f65bc6fe30c7758ccab66a642edef6636763b0f
Qiyao Tan <qt2138@columabi.edu>
2021-04-01 10:57:55 +0000

    new changes

--- adopt/urls.py
+++ adopt/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'adopt'
 
 urlpatterns = [
         path('',views.index),
+        path('<int:pet_id>/',views.detail, name = 'detail')
         ]
--- adopt/views.py
+++ adopt/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from .models import Pet
 
 def index(request):
-    return render(request,'adopt/index.html',{})
+    pets = Pet.objects.all()
+    context = {
+            'pets':pets,
+            }
+    return render(request,'adopt/index.html',context)
+
+def detail(request,pet_id):
+    pet = get_object_or_404(Pet, pk = pet_id)
+    context = {
+            'pet':pet,
+            }
+    return render(request,'adopt/detail.html',context)
